dbf1aa55bec82390b9bc1ee655f34ce77de25d26
Muhaz <muhazvla313@gmail.com>
2022-09-29 12:44:41 +0530

    updated Models

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const bodyParser=require("body-parser")
+
+// const route=require('./route/route')
+
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://muhaz:6VE8Lk82R6vAuBok@cluster0.syf7fzi.mongodb.net/group1database",{useNewUrlParser:true})
+.then(()=>{
+    console.log("MongoDB Connected..")
+}).catch(err=>{
+    console.log(err.message);
+})
+
+// app.use('/',route)
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))})
