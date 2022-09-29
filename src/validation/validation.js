27505bd37541089708fa161f9489e982fe7536b3
Muhaz <muhazvla313@gmail.com>
2022-09-29 13:27:36 +0530

    src

---
+++ src/validation/validation.js
@@ -0,0 +1,10 @@
+const isValid = function(value){
+    if(typeof value !== "string") return false
+    if (typeof value === "string" && value.trim().length ===0) return false
+    return true;
+}
+const isValidRequestBody = function(requestBody){
+    return Object.keys(requestBody).length > 0
+}
+
+module.exports = {isValid, isValidRequestBody}
