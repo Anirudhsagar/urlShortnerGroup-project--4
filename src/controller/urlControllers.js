9d6389f46ab951b1045d2d1e76a17be88ec4f8f4
Muhaz <muhazvla313@gmail.com>
2022-10-03 18:50:50 +0530

    redis

--- src/controller/urlControllers.js
+++ src/controller/urlControllers.js
@@ -1,8 +1,7 @@
 const urlModel = require("../models/urlModel");
 const shortid = require("shortid");
-const axios = require("axios");
+// const axios = require("axios");
 const validator = require("../validation/validation");
-
 
 const redis = require("redis");
 
@@ -11,11 +10,11 @@ const { stat } = require("fs");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  18447,
-  "redis-18447.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  18447,    //port number
+  "redis-18447.c212.ap-south-1-1.ec2.cloud.redislabs.com", //end point
   { no_ready_check: true }
 );
-redisClient.auth("s38aevI8u89pQakUvsUPMdV2Spaw4GoV", function (err) {
+redisClient.auth("s38aevI8u89pQakUvsUPMdV2Spaw4GoV", function (err) {     //password
   if (err) throw err;
 });
 
@@ -23,12 +22,6 @@ redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
 
-
-
-//1. connect to the server
-//2. use the commands :
-
-//Connection setup for redis
 
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
@@ -41,66 +34,64 @@ function checkUrl(url) {
 }
 const urlShortener = async function (req, res) {
   try {
-    // if (!isValidBody(req.body)) return res.status(400).send({ status: false, message: "Provide the longUrl in the body." })
-
     let longUrl = req.body.longUrl;
-  
-    
+
     if (!validator.isValid(longUrl))
       return res
         .status(400)
         .send({ status: false, message: "Provide the longUrl in the body." });
-    
 
-    let found = false;
-
-    await axios
-      .get(longUrl)
-      .then((response) => {
-        if (response.status == 200 || response.status == 201) found = true;
-        if (!checkUrl(longUrl))
-          return res
-            .status(400)
-            .send({ status: false, message: "Enter a valid URL." });
-      })
-
-      .catch((error) => {status : false});
+    if (!checkUrl(req.body.longUrl)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Enter a valid URL." });
+    }
 
     let urlPresent = await urlModel
       .findOne({ longUrl: longUrl })
       .select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 });
     if (urlPresent)
-      return res
-        .status(200)
-        .send({
-          status: true,
-          message: "urlCode is already generated for this URL.",
-          data: urlPresent,
-        });
+      return res.status(200).send({
+        status: true,
+        message: "urlCode is already generated for this URL.",
+        data: urlPresent,
+      });
 
-    let urlCode = shortid.generate(longUrl).toLowerCase()
-    let shortUrl = "http://localhost:3000/" + urlCode;
-    obj = {
-      longUrl: longUrl,
+    let urlCode = shortid.generate(longUrl).toLowerCase();
+    if ((longUrl)) {
+      let cachedUrl = await GET_ASYNC(`${longUrl}`)
+      cachedUrl = JSON.parse(cachedUrl)
+      if(cachedUrl)    
+      return res.status(200).send({ status: true, data: cachedUrl})
 
-      urlCode: urlCode,
-      shortUrl: shortUrl,
-    };
+      let url = await urlModel.findOne({longUrl}).select({ _id: 0, __v: 0 })
 
-    let data = { longUrl: longUrl, shortUrl: shortUrl, urlCode: urlCode };
+      if (url) {
+          await SET_ASYNC(`${longUrl}`, JSON.stringify(url))
+          res.status(200).send({ status: true, data: url })
+      } else {
 
-    let urlGenerated = await urlModel.create(data);
-    let result = {
-      longUrl: urlGenerated.longUrl,
-      shortUrl: urlGenerated.shortUrl,
-      urlCode: urlGenerated.urlCode,
-    };
-    return res.status(201).send({ status: true, data: result });
-  } catch (err) {
-    return res.status(500).send({ status: false, message: err.message });
+          const shortUrl = "http://localhost:3000/" + urlCode
+
+          let url = await urlModel.create({longUrl, shortUrl, urlCode})
+          let data = {
+              longUrl : url.longUrl,
+              shortUrl : url.shortUrl,
+              urlCode : url.urlCode
+          }
+
+          res.status(201).send({ status: true, data: data })
+      }
   }
-};
+  else {
+      res.status(400).send({ status: false, message: "Invalid longUrl" })
+  }
+}
+catch (error) {
+  res.status(500).send({ status: false, message: error.message })
+}}
 // ======================get API=====================
+
 const getUrlcode = async function (req, res) {
   try {
     let data = req.params.urlCode;
@@ -116,43 +107,17 @@ const getUrlcode = async function (req, res) {
         .status(404)
         .send({ status: false, msg: "UrlCode is not found" });
     }
+    let cachedata = await GET_ASYNC(`${data}`);
+    if (cachedata) {
+      return res.status(302).redirect(data);
+    } else {
+      let profile = await urlModel.findOne({ data });
+      await SET_ASYNC(`${data}`, JSON.stringify(profile));
+    }
     return res.status(302).redirect(Urlcodefound.longUrl);
   } catch (error) {
     res.status(500).send({ status: false, msg: error.message });
   }
 };
 
-
-const getUrlcode = async function (req,res){
-try {
-  let data = req.params.urlCode
-    if(!shortid.isValid(data)){
-    return res.status(400).send({status : false , msg : "please give value in the param"})}
-    //  if(!shortid.isValid(data)){
-    //   return res.status(400).send({status : false , msg : "Invalid Urlcode "})}
-      let Urlcodefound = await urlModel.findOne({urlCode:data})
-      if(!Urlcodefound){
-        return res.status(404).send({status : false , msg : "UrlCode is not found"})
-      }
-      let cachedata = await GET_ASYNC(`${data}`)
-      if(cachedata){
-        res.status(302).redirect(data)
-      }else{
-        let profile = await urlModel.findOne({data})
-        await SET_ASYNC (`${data}`, JSON.stringify(profile))
-      }
-      return res.status(302).redirect(Urlcodefound.longUrl)
-
-} catch (error) {
-  res.status(500).send({status : false , msg : error.message})
-}
-}
-
-
-// ### GET /:urlCode
-// - Redirect to the original URL corresponding
-// - Use a valid HTTP status code meant for a redirection scenario.
-// - Return a suitable error for a url not found
-// - Return HTTP status 400 for an invalid request
-module.exports = { urlShortener ,getUrlcode};
 module.exports = { urlShortener, getUrlcode };
